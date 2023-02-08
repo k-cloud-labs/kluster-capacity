7e7e66f0c05f98ffbbb04be6240e2a70728a7af9
kaku <1154584512@qq.com>
2023-02-08 14:54:31 +0800

    fix: namespacenames nil bug
    Signed-off-by: kaku <1154584512@qq.com>

--- app/cmds/capacityestimation/options/namespacename.go
+++ app/cmds/capacityestimation/options/namespacename.go
@@ -8,23 +8,23 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type NamespaceNames []*NamespaceName
+type NamespaceNames []NamespaceName
 
 type NamespaceName struct {
 	Namespace string `json:"namespace"`
 	Name      string `json:"name"`
 }
 
-func (n NamespaceNames) Set(nns string) error {
+func (n *NamespaceNames) Set(nns string) error {
 	for _, nn := range strings.Split(nns, ",") {
 		nnStrs := strings.Split(nn, "/")
 		if len(nnStrs) == 1 {
-			n = append(n, &NamespaceName{
+			*n = append(*n, NamespaceName{
 				Namespace: metav1.NamespaceDefault,
 				Name:      nnStrs[0],
 			})
 		} else if len(nnStrs) == 2 {
-			n = append(n, &NamespaceName{
+			*n = append(*n, NamespaceName{
 				Namespace: nnStrs[0],
 				Name:      nnStrs[1],
 			})
@@ -36,15 +36,15 @@ func (n NamespaceNames) Set(nns string) error {
 	return nil
 }
 
-func (n NamespaceNames) String() string {
+func (n *NamespaceNames) String() string {
 	strs := []string{}
-	for _, nn := range n {
+	for _, nn := range *n {
 		strs = append(strs, fmt.Sprintf("%s/%s", nn.Namespace, nn.Name))
 	}
 
 	return strings.Join(strs, ",")
 }
 
-func (n NamespaceNames) Type() string {
+func (n *NamespaceNames) Type() string {
 	return "NamespaceNames"
 }
